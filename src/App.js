eb49c664eb2e528a5da92cf4a4ab32d63cc5e2dd
PINNINTI TEJA SREE <152084449+tejasree5@users.noreply.github.com>
2024-12-08 02:37:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DashboardLayoutBasic from './components/DashboardLayoutBasic';
-import navbar from './components/Navbar.js';
+
 import PrivateRoute from './components/PrivateRoute';  // PrivateRoute is used for protected routes
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/dashboard" element={<PrivateRoute><DashboardLayoutBasic /></PrivateRoute>} />
-        <Route path="/navbar"  element={<PrivateRoute><Navbar /></PrivateRoute>} />
+      
       </Routes>
     </Router>
   );
